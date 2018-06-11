47710c47f13e5110e4d77e861a988ffc7ca8ea3e
Reet <reet1plus1@gmail.com>
2018-06-12 01:36:31 +0530

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipes: Recipe[] = [
+  new Recipe('A test recipe', 'This is a test desc' , 'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg'),
+  new Recipe('A test recipe', 'This is a test desc' , 'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg')
+];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
